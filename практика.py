9d9baeed0cb18116353e23d3f02fcc159fbc9b5c
Daniel-data-analyse <aulbekovdaniel8@gmail.com>
2024-12-01 12:02:23 +0600

    commits

--- практика.py
+++ практика.py
@@ -1,12 +1,21 @@
-name = input("enter your first name: ")
-surname = input("enter your last name: ")
-age = int(input("enter your age: "))
-come_from = input("enter where are you from: ")
-if age == 1:
-    print(f"Your full name is {surname} {name}. You're {age} year old and you are from {come_from}. Your are a baby")
-if 18 > age > 1:
-    print(f"Your full name is {surname} {name}. You're {age} years old and you are from {come_from}. You are a teenager")
-if age >= 18:
-    print(f"Your full name is {surname} {name}. You're {age} years old and you are from {come_from}. You are an adult")
+while True:
+    player = float(input("enter a temperature: "))
+    player1 = input("enter a temperature unit: ").capitalize()
+    tries = 1
+    def Celsius_calculator():
+        if player1 == "Fahrenheit":
+            print(f"{player} {player1} temperature in Celsius is {(player - 32) *(5/9)}C")
+        if player1 == "Kelvin":
+           print(f"{player} {player1} temperature in Celsius is {player - 273.15}C")
+        if player1 == "Reaumur":
+           print(f"{player} {player1} temperature in Celsius is {player *(5/4)}C")
+        if player1 == "Celsius":
+           print("Your input is already in Celsius!")
+           tries = tries + 1
+           if tries == 3:
+            print("Are you a stupid guy?")
+        if player1 != "Kelvin" and player1 != "Reaumur" and player != "Fahrenheit" and player1 != "Celsius":
+            print("Invalid input")
+            print("Please, try again!")
 
-
+    Celsius_calculator()
