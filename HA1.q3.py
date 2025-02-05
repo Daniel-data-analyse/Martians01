9d53243a845246ac4bee636bb40842b30c432f98
Daniel-data-analyse <aulbekovdaniel8@gmail.com>
2025-02-05 20:08:11 +0600

    h1

---
+++ HA1.q3.py
@@ -0,0 +1,9 @@
+user = input("What is the user's name? ")
+age = input("What is the user's age? ")
+country = input("What is the user's country of birth? ")
+known = input( "What is the user known for? ")
+
+print("What is the user's name? ", user,
+      "What is the user's age? ",  age,
+      "What is the user's country of birth? ", country,
+      "What is the user known for? ", known, end = " ")
